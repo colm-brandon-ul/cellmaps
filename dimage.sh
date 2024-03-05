2d23bb0c5c03494c3ac97fbf9558d192b7031c81
Colm Brandon <colm.brandon@ul.ie>
2024-03-05 17:38:16 +0000

    was using zsh commands that mean nothing in bash, corrected it

--- dimage.sh
+++ dimage.sh
@@ -1,8 +1,13 @@
-for dockerfile in $(find ./services -name Dockerfile); do;  
-    dir_of_image=$(dirname $dockerfile);
-    image_name=$dir_of_image:t
+for dockerfile in $(find ./services -name Dockerfile); do
+    dir_of_image=$(dirname $dockerfile)
+    last_folder=$(basename $dir_of_image)
+    
+    # echo "Building $dir_of_image"
+    image_name=$last_folder
+    # echo "Building $image_name"
 
     image_name=${image_name//\//-}
     docker_image_name="${image_name//[^a-zA-Z0-9]/-}"
-    docker buildx build --platform linux/amd64,linux/arm64 -t $docker_image_name --push $dir_of_image
+    echo "Building $docker_image_name"
+    # docker buildx build --platform linux/amd64,linux/arm64 -t $docker_image_name --push $dir_of_image
     done
