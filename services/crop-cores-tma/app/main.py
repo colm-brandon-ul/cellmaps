233e774fdaedaf4a98404b24fb518d92bf86d376
Colm Brandon <colm.brandon@ul.ie>
2025-02-20 17:43:43 +0000

    tidy up on cctma

--- services/crop-cores-tma/app/main.py
+++ services/crop-cores-tma/app/main.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
 from enum import Enum
-
 from cdb_cellmaps.data import DearrayedTissueMicroArray, RegionsOfInterest, TissueCore, TissueCoreProteinChannel, TissueMicroArray
 from cdb_cellmaps import data_utils
 from cdb_cellmaps.process import Automated, DeArray
-
 from image_utils import coordinate_translation #type: ignore
 
 
@@ -17,15 +15,12 @@ class CropCoresTMAProcessInput:
             dearrayed_tissue_micro_array: bool
         # core_equalised_image_split_dir: str
         data_flow: DataFlow
-
     @dataclass
     class WorkflowParameters:
         rois: RegionsOfInterest
-
     @dataclass
     class Data:
         tissue_micro_array: TissueMicroArray
-
     system_parameters: SystemParameters
     workflow_parameters: WorkflowParameters
     data: Data
@@ -35,7 +30,6 @@ class CropCoresTMAProcessInput:
 class CropCoresTMAProcessOutput:
     class Control(str, Enum):
         success = 'success'
-
     @dataclass
     class Data:
         dearrayed_tissue_micro_array: DearrayedTissueMicroArray
@@ -47,19 +41,13 @@ class CropCoresTMAProcessOutput:
 
 class CropCoresTMA(DeArray,Automated):
     _ROUTING_KEY = 'CropCoresTMA'
-
     def __init__(self) -> None:
         super().__init__()
 
     def deserialize_process_input(self,body) -> CropCoresTMAProcessInput:
         return data_utils.decode_dict(data_class=CropCoresTMAProcessInput,data=body)
-
-        
     def process(self, prefix, input: CropCoresTMAProcessInput) -> CropCoresTMAProcessOutput:
-        
         temp = DearrayedTissueMicroArray()
-        
-        
         for channel_name,channel in input.data.tissue_micro_array.items():
             # Load in full channel
             tiff = channel.read()
@@ -67,7 +55,6 @@ class CropCoresTMA(DeArray,Automated):
             # This will iterate correctly, but need to figure out how to retain type annotations for objects
             # being iterated over
             for i,roi in enumerate(input.workflow_parameters.rois):
-                
                 # Translate to coordinates from the image they were predicted/draw on to the full size image
                 bounds = coordinate_translation(roi.img_w, 
                                              tiff.width, 
@@ -77,11 +64,9 @@ class CropCoresTMA(DeArray,Automated):
                                              roi.y1,
                                              roi.x2,
                                              roi.y2)
-                
                 core_name = f'A{i}'
                 if core_name not in temp.keys():
                     temp[core_name] = TissueCore()
-
                 # Change Prefix
                 temp[core_name][channel_name] = TissueCoreProteinChannel.write(
                     data=tiff.crop(bounds), 
@@ -89,12 +74,12 @@ class CropCoresTMA(DeArray,Automated):
                         level=core_name),
                         file_name=channel_name
                         )
-
         return CropCoresTMAProcessOutput(
             data=CropCoresTMAProcessOutput.Data(
                 dearrayed_tissue_micro_array=temp
             )
         )
+
 
 if __name__ == "__main__":
     CropCoresTMA().run()
